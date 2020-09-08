fd848ef090a30f55718d4163325928c77fe4374a
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-09-08 12:51:32 -0300

    Update index.js

--- Base/index.js
+++ Base/index.js
@@ -1,98 +1,73 @@
-const Database = require('../Database/Connection');
-const queryBuilder = require('../Database/Utils').queryBuilder;
 
 class Base {
 	constructor(tableName){
 	this.id = null; 
 	this._tableName = tableName;
-	this._db = Database.getConnection();
+	this._columnsInverse = {ID:'id'}
 	this._columns = {id: 'ID'};
-	}
+	this._defaults = {  };
+	}	
 
+	getColumn(property) {
+		return this._columns[property];
+	}
 	
-	_setValues(vals) {
-		for(key of vals){
-			this._colunas[key] = vals[key];
+	_setValues(vals, fromDatabase) {
+		if(fromDatabase){
+			for(let key in vals){
+				if(this._columnsInverse[key]) {
+					this[key] = vals[key];
+				}	
+			}
+		} else {
+			for(let key in vals){
+				if(this._columns[key] !== "undefined") {
+					this[key] = vals[key];
+				}	
+			}
 		}
+
+		for(let defs in this._defaults){
+			if((typeof this[defs] === "undefined")) {
+				this[defs] = this._defaults[defs];
+			}	
+		}
+		
 	}
 
-	_addColumn(property, column){
+	_addColumn(property, column, defaultVal) {
 		this._columns[property] = column;
+		this._columnsInverse[column] = property;
+		if((typeof defaultVal !== "undefined")) this._defaults[property] = defaultVal;
 	}
 
 	_getColumn(property) {
 		return this._columns[property];
 	}
-
-	getColumn(property) {
-		return this._columns[property];
-	}
-	_persistObject () {
+	
+	
+	_createUpdateObject() {
 		let obj = {};
 
-		for(var col in this._columns) {
-			obj[this._columns[col]] = this[col];
-		}
-		if(obj[this._columns.id]===null){
-			delete obj[this._columns.id];
+		//Get all value and set it in a Column name variables
+		for (var col in this._columns) {
+			if(!(typeof this[col] === "undefined" )) {
+				obj[this._columns[col]] = this[col];
+			}
 		}
 		return obj;
 	}
+	
+	_createPersistObject() {
+		let obj = {};
 
-	async _persist(persistObject){	
-		try {
-		const res = await this._db.query(persistObject.query, persistObject.values);
-			return res;
-		} catch(err) {  
-			this.logger.error(err);
-			err.erro = 'ERRO';
-			return err;
+		for (var col in this._columns) {
+			obj[this._columns[col]] = this[col];
 		}
-	}
-
-	async get(id) {
-		const res = await this._db.query(`SELECT * FROM ${this._tableName} where ${this._columns.id} = $1`, []);
-		let response = await res.rows[0];
-        return response || {};
-	}
-	async save() 
-	{
-		let obj = this.persistObject()
-		let toPersist = queryBuilder.insert(this._tableName, obj);
-		return await this.persist(toPersist);
-	}
-	async update() {
-		let obj = this.persistObject()
-		let toPersist = queryBuilder.update(this._tableName, obj);
-		return await this.persist(toPersist);
-	}
-
-	static async deleteById(id) {
-		let query =  `DELETE ${this._tableName} where ${this._columns.id} = $1`
-		const res = await this._db.query(query, [id]);
-        let response = await res.rows[0];
-        return response || {};
-	}
-	async delete() {	
-        return await this.deleteById(this.id);
-	}
-
-	static async list() 
-	{
-		const res = await this._db.query(`SELECT * FROM ${this._tableName}`, []);
-		let response = await res.rows;
-        return response ;
-		
-	}
-	static async search (options) {
-		const params = queryBuilder.search(new this(), options);
-		const res = await this._db.query(params.query, params.values);
-		let response = await res.rows;
-        return response ;
-	}
-	static async paginate(options) 
-	{
-			//TODO
+		if (obj[this._columns.id] === null) {
+			delete obj[this._columns.id];
+		}
+		return obj;
 	}
 }
 module.exports = Base;
