d2e635d3cab3e03da3a062e24bdda5400ca6380b
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-09-08 08:30:30 -0300

    Update index.js

--- Database/Config/index.js
+++ Database/Config/index.js
@@ -1,1 +1,20 @@
 
+module.exports = (() => {
+    const dotenv = require('dotenv');
+    const logger = require('../utils/logger');
+  
+    const options = {
+        user: process.env.DB_USER,
+        host: process.env.DB_URL,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: 5432,
+        ssl: true
+    };
+  
+    const createTables = (client) => {
+    }
+    return {
+       options, createTables
+    }
+})();
