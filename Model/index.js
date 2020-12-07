f2a5ff2dcc6001e1c46b6bb0f761fe54469efaf1
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-07 11:14:32 -0300

    Update index.js

--- Model/index.js
+++ Model/index.js
@@ -36,13 +36,13 @@ class Model {
 		
 	}
 
-	_addColumn(property, column, defaultVal) {
+	addColumn(property, column, defaultVal) {
 		this._columns[property] = column;
 		this._columnsInverse[column] = property;
 		if((typeof defaultVal !== "undefined")) this._defaults[property] = defaultVal;
 	}
 
-	_getColumn(property) {
+	getColumn(property) {
 		return this._columns[property];
 	}
 	
